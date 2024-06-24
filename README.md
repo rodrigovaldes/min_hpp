c3733bd727e6fdb28f4a30c0b3059242497de349
rodrigo valdes <rodrigovaldes@uchicago.edu>
2024-06-24 15:46:24 -0700

    closing working version

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Minimum Heroku Python Postgress, with full python (sqlalchemy) implementation
 + Understand flask db migrate
 + Comprehend alembic
 + Long term reference on how to use databases in Flask
++ Use a basic scheduler
 
 ## Virtual environment
 Change directory to local repo root. NOTE: venv is the standard name of a
@@ -20,18 +21,19 @@ pip install -r requirements.txt
 Database creation is not made in the APP. It's necessary to create the database
 before the app deployment. Create a database on the CLI
 
-1. Setup db and create username and password to access the db
+1. Setup db and create username and password to access the db.
 ```
 psql postgres
 CREATE DATABASE <database_name>;
 CREATE USER <username> WITH PASSWORD '<password>';
 GRANT ALL PRIVILEGES ON DATABASE <database_name> TO <username>;
 ```
-### DELTE THIS postgres=# CREATE USER rhpp  WITH PASSWORD 'hpp1234!@#$'; ###
-
 2. replace the value in .env file for DATABASE_URL to your db 
 ```
 DATABASE_URL=postgresql://{username}:{password}@localhost:5432/{dbname}
 ```
+
+## Install the SQLAlchemy
+`pip3 install Flask-SQLAlchemy`
 
 
--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 # source: https://www.geeksforgeeks.org/flask-creating-first-simple-application/
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_apscheduler import APScheduler
 from flask_migrate import Migrate
-from db_test import db
+# from db_test import db
 import os
 from dotenv import load_dotenv
+from utils import print_message
 load_dotenv()
 
-app = Flask(__name__)
-
+# Get constants from the environment
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
+# Initialize the app
+app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+scheduler = APScheduler()
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/other')
+def other():
+    return render_template('other.html')
+
+# Add the tables
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
@@ -34,3 +46,18 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, nullable=False)
     min_transactions = db.Column(db.Integer)
+
+if __name__ == '__main__':
+   
+   # scheduler.add_job(func=print_message, trigger='interval', seconds=2, id='printer')
+   scheduler.add_job(func=print_message,
+                     trigger='cron',
+                     day_of_week='mon-sun',
+                     hour=15,
+                     minute=27,
+                     id='printer')
+   scheduler.start()
+
+   # app.run(debug=False, host='0.0.0.0', use_reloader=False) ## USE RELOADER?
+   app.run(debug=False, host='0.0.0.0')
+   
